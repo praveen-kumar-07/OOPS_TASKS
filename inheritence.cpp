d6329d995f437d246d69352a2653094467023d47
Praveen Kumar <119918367+praveen-kumar-07@users.noreply.github.com>
2023-09-21 21:48:11 +0530

    Create inheritence.cpp

---
+++ inheritence.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define ll long long
+#define pb push_back
+using namespace std;
+
+
+//Order of Execution of Constructors and Destructors in inheritance // 
+class Person{
+	 public:
+	 	Person(){
+	 		cout<<"Constructor of Base class is called"<<endl;
+	 	}
+	 	~Person(){
+	 		cout<<"Destructor of Base class is called"<<endl;
+	 	}
+};
+class Student : public Person{
+public:
+	Student(){
+		cout<<"Constructor of Drived class is called"<<endl;
+	}
+	~Student(){
+		cout<<"Destructor of Drived class is called"<<endl;
+	}
+};
+int main() {
+    // your code goes here
+    
+    Student Ashok;
+    
+
+
+}
